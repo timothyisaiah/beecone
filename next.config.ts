bebe000f5615a19ba759dbbcef88c77ff1234a60
timothyisaiah <timothyisaiah7@gmail.com>
2026-03-17 16:25:41 +0300

    gh-pages deployement

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGhPages = process.env.GITHUB_ACTIONS === "true";
+const repoName = process.env.NEXT_PUBLIC_REPO_NAME || "";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isGhPages ? `/${repoName}` : "",
+  assetPrefix: isGhPages ? `/${repoName}/` : "",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -9,6 +16,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
