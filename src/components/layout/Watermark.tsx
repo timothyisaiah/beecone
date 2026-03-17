258dd808cb0775b00ca2b905d72d058bf3da7193
timothyisaiah <timothyisaiah7@gmail.com>
2026-03-17 17:49:00 +0300

    Asset water mark

--- src/components/layout/Watermark.tsx
+++ src/components/layout/Watermark.tsx
@@ -1,8 +1,17 @@
+import logoDark from "@/../public/images/logo-dark.png";
+import logoGold from "@/../public/images/logo-gold.png";
+
 export default function Watermark() {
   return (
     <div className="watermark-overlay" aria-hidden="true">
-      <div className="watermark-layer watermark-dark" />
-      <div className="watermark-layer watermark-gold" />
+      <div
+        className="watermark-layer watermark-dark"
+        style={{ backgroundImage: `url(${logoDark.src})` }}
+      />
+      <div
+        className="watermark-layer watermark-gold"
+        style={{ backgroundImage: `url(${logoGold.src})` }}
+      />
     </div>
   );
 }
